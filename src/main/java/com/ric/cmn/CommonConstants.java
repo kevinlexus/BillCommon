198e9ae57b6bd77de36d0375d2d1508b69d871b4
LeoLexx <factorkevin@gmail.com>
2019-02-05 07:14:34 +0300

    Доработка WebController-2

---
+++ src/main/java/com/ric/cmn/CommonConstants.java
@@ -0,0 +1,20 @@
+package com.ric.cmn;
+
+import java.math.BigDecimal;
+
+/**
+ * Общие константы
+ * @version 1.1
+ */
+public interface CommonConstants {
+
+    // маркер, для проверки необходимости остановки потоков начисления и прочего
+    // (только если весь фонд задан для расчета)
+    static final String stopMark = "processMng.genProcess";
+
+    // норматив по эл.энерг. ОДН в доме без лифта на м2 общ.имущ.
+    static final BigDecimal ODN_EL_NORM = new BigDecimal("2.7");
+    // норматив по эл.энерг. ОДН в доме с лифтом на м2 общ.имущ.
+    static final BigDecimal ODN_EL_NORM_WITH_LIFT = new BigDecimal("4.1");
+
+}
