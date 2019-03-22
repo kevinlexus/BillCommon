26b27b73bbea8bf1bd7891ecf984162a8dc74383
LeoLexx <factorkevin@gmail.com>
2019-03-22 07:13:15 +0300

    Исправление работы Итогового формирования

--- src/main/java/com/ric/cmn/CommonConstants.java
+++ src/main/java/com/ric/cmn/CommonConstants.java
@@ -11,6 +11,8 @@ public interface CommonConstants {
     // маркер, для проверки необходимости остановки потоков начисления и прочего
     // (только если весь фонд задан для расчета)
     static final String stopMark = "processMng.genProcess";
+    // маркер для остановки процесса итогового формирования
+    static final String stopMarkAmntGen = "AmountGeneration";
 
     // норматив по эл.энерг. ОДН в доме без лифта на м2 общ.имущ.
     static final BigDecimal ODN_EL_NORM = new BigDecimal("2.7");
